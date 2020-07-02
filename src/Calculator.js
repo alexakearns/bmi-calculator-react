9ca07c99e5c54b3aa9f1c17e1b90e998db7cec29
Alexa <alexakearns@outlook.com>
2020-07-02 17:55:53 +0100

    update calculator

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,12 +3,26 @@ import React, { Component } from 'react'
 export class Calculator extends Component {
   constructor(props) {
     super(props)
-    this.state = {bmi: "normal"}
+    this.state = {
+      height: 168,
+      weight: 62,
+      bmi: 22.0,
+      bmiClass: "normal"
+    }
   }
+  heightChange = (height) => {
+    this.setState({ height: height })
+  } 
   render() {
     return (
       <div>
         <h1>BMI Calculator</h1>
+        <p>Enter your height:</p>
+        <input
+        type="text"
+        name="height"
+        ></input>
+        <h3>Your height is {this.state.height}!</h3>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Calculator from './Calculator';
 
-ReactDOM.render(<Calculator />, document.getElementById('root'));
+ReactDOM.render(<Calculator bmi="Overweight"/>, document.getElementById('root'));
